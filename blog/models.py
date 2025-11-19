58adf1a3a0fbc756f28488cb33415a187ecbd156
Zxi01 <cameron.m.jennings@hotmail.com>
2025-11-19 09:44:55 +0000

    tidying up

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,9 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model):
+    """
+    Stores a single blog post to :model:`blog.Post`.
+    """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
